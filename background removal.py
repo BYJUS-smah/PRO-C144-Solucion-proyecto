0059b741bb84430e0030e81fad1ed9aa3f950130
BYJUS-smah <95708613+BYJUS-smah@users.noreply.github.com>
2022-07-27 14:23:08 -0500

    Update background removal.py

--- background removal.py
+++ background removal.py
@@ -1,59 +1,59 @@
-# import cv2 to capture videofeed
+# Importar cv2 para capturar el video.
 import cv2
 
 import numpy as np
 
-# attach camera indexed as 0
+# Establecer el índice de cámara como 0.
 camera = cv2.VideoCapture(0)
 
-# setting framewidth and frameheight as 640 X 480
+# Establecer el ancho y altura del cuadro como 640 X 480.
 camera.set(3 , 640)
 camera.set(4 , 480)
 
-# loading the mountain image
+# Cargar la imagen de la montaña.
 mountain = cv2.imread('mount everest.jpg')
 
-# resizing the mountain image as 640 X 480
+# Ajustar el tamaño de la imagen de la montaña a 640 X 480.
 mountain = cv2.resize(mountain , (640 , 480))
 
 while True:
 
-    # read a frame from the attached camera
+    # Leer el cuadro desde la cámara establecida.
     status , frame = camera.read()
 
-    # if we got the frame successfully
+    # Si obtenemos el cuadro exitosamente.
     if status:
 
-        # flip it
+        # Lo volteamos.
         frame = cv2.flip(frame , 1)
 
-        # converting the image to RGB for easy processing
+        # Convertir la imagen a RGB para un procesamiento sencillo.
         frame_rgb = cv2.cvtColor(frame , cv2.COLOR_BGR2RGB)
 
-        # creating thresholds
+        # Crear límites.
         lower_bound = np.array([100,100,100])
         upper_bound = np.array([255,255,255])
 
-        # thresholding image
+        # Poner límites a la imagen.
         mask = cv2.inRange(frame_rgb, lower_bound, upper_bound)
 
-        # inverting the mask
+        # Invertir la máscara.
         mask = cv2.bitwise_not(mask)
 
-        # bitwise and operation to extract foreground / person
+        # Operación de bit a bit y operación para extraer el primer plano / persona.
         person = cv2.bitwise_and(frame, frame , mask = mask)
 
-        # final image
+        # Imagen final.
         final_image = np.where(person  ==  0 , mountain , person)
 
-        # show it
+        # Mostrar la imagen final.
         cv2.imshow('frame' , final_image)
 
-        # wait of 1ms before displaying another frame
+        # Esperar 1ms antes de mostrar otro cuadro.
         code = cv2.waitKey(1)
         if code  ==  32:
             break
 
-# release the camera and close all opened windows
+# Soltar la cámara y cerrar todas las ventanas abiertas.
 camera.release()
 cv2.destroyAllWindows()
